90af95bb1233d2d33a976a8c5ad3f770662f9e40
AndrewHLau <61214225+AndrewHLau@users.noreply.github.com>
2020-03-21 16:49:05 -0400

    Update main.py

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,3 +1,27 @@
 # Store the file path associated with the file (note the backslash may be OS specific)
 file = '../Resources/budget_data.csv'
 
+print("Financial Analysis")
+print("----------------------------------")
+
+#The total number of months included in the data set
+#Count the number of lines
+
+print ("Total Months:")
+
+#The net total amount of profit/Losses over the entire period
+#sum the profits
+print ("Total:")
+
+#The average of the changes in Profit/Losses over the entire period
+
+#mean the profits
+print ("Average Change:")
+
+
+#The greatest increase in profits (date and amount) over the entire period
+print ("Greatest Increase in Profits")
+
+
+#The greatest decrease in losses (date and amount) over the entire period
+print ("Greatest Decrease in Profits")
