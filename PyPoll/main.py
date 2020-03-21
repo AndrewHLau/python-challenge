9f01417b5eb0fbba301c2a756844bda1aa711823
AndrewHLau <61214225+AndrewHLau@users.noreply.github.com>
2020-03-21 17:18:09 -0400

    Update main.py

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -17,7 +17,8 @@ print ("Total Votes:3521001")
 print ("--------------------------------")
 
 #A complete list of candidates who received votes
-#The percentage of votes each candidate won
+#The percentage of votes each candidate won, count the number of votes each candidate received
+# Divide by total votes and multiply by 100
 #The winner of the election based on popular vote
 print ("Khan: 63.000% (2218231)")
 print ("Correy: 20.000% (704200)")
@@ -25,7 +26,7 @@ print ("Li: 14.000% (492940)")
 print ("O'Tooley: 3.000% (105630)")
 print ("--------------------------------")
 
-#The winner of the election based on popular vote
+#The winner of the election based on popular vote, print the candidate that received the most votes
 print ("Winner: Khan")
 print ("--------------------------------")
 
