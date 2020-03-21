b5f18af67b1a12c874afe8e4f32ee8ac6aef6b88
AndrewHLau <61214225+AndrewHLau@users.noreply.github.com>
2020-03-21 16:57:03 -0400

    Update main.py

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,3 +1,8 @@
+# Dependencies
+import os
+import csv
+
+
 # Store the file path associated with the file (note the backslash may be OS specific)
 file = '../Resources/budget_data.csv'
 
@@ -20,8 +25,11 @@ print ("Average Change:")
 
 
 #The greatest increase in profits (date and amount) over the entire period
-print ("Greatest Increase in Profits")
+print ("Greatest Increase in Profits:")
 
 
 #The greatest decrease in losses (date and amount) over the entire period
-print ("Greatest Decrease in Profits")
+print ("Greatest Decrease in Profits:")
+
+# Specify the file to write to
+output_path = os.path.join("..", "output", "new.csv")
