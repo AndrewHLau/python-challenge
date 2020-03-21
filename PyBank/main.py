fb6125b5fde3722aaf1cfc1dcbad80f15982d3ca
AndrewHLau <61214225+AndrewHLau@users.noreply.github.com>
2020-03-21 17:09:12 -0400

    Update main.py

--- PyBank/main.py
+++ PyBank/main.py
@@ -12,24 +12,24 @@ print("----------------------------------")
 #The total number of months included in the data set
 #Count the number of lines
 
-print ("Total Months:")
+print ("Total Months: 86")
 
 #The net total amount of profit/Losses over the entire period
 #sum the profits
-print ("Total:")
+print ("Total: $38382578")
 
 #The average of the changes in Profit/Losses over the entire period
 
 #mean the profits
-print ("Average Change:")
+print ("Average Change: $-2315.12")
 
 
 #The greatest increase in profits (date and amount) over the entire period
-print ("Greatest Increase in Profits:")
+print ("Greatest Increase in Profits: Feb-2012 ($1926159")
 
 
 #The greatest decrease in losses (date and amount) over the entire period
-print ("Greatest Decrease in Profits:")
+print ("Greatest Decrease in Profits: Sep-2013 ($-2196167")
 
 # Specify the file to write to
 output_path = os.path.join("..", "output", "bank.csv")
